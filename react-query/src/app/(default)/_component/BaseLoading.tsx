757be43d6bcdb2da7884238458087e047f10c572
정승현 <armyso9506@plateer.com>
2024-01-02 17:02:47 +0900

    React-Query init

---
+++ react-query/src/app/(default)/_component/BaseLoading.tsx
@@ -0,0 +1,10 @@
+import style from "./baseLoading.module.css"
+
+
+export default function BaseLoading() {
+    return (
+        <div className={ style.container }>
+            Loading...
+        </div>
+    )
+}
--- react-query/src/app/(default)/movie/_component/MovieList.tsx
+++ react-query/src/app/(default)/movie/_component/MovieList.tsx
@@ -6,18 +6,24 @@ import getMovieList from "@/app/(default)/movie/_lib/getMovieList";
 import {Movie} from "@/models/Movie";
 import {faker} from "@faker-js/faker";
 import style from "./movieList.module.css"
+import BaseLoading from "@/app/(default)/_component/BaseLoading";
 
 
 export default function MovieList() {
-    const { data } = useQuery<Movie[]>({
+    const { data, isLoading, isError, error } = useQuery<Movie[]>({
         queryKey: ['movie', 'list'],
         queryFn: getMovieList,
 
         staleTime: 60 * 1000, // 캐시 유지 타임 / 기본값 0 이며 fresh -> stale / 60 * 1000 = 1분
         // staleTime: Infinity // 항상 fresh 상태 값을 가져오지 않는다
         // gcTime: 300 * 1000 // 삭제 타임 / 기본값 5분 inactve 에서 5분이 지나면 삭제 / staleTime 이 gcTime 보다 무조건 작아야 한다.
-        initialData: () => [] // 초기 데이터
+        initialData: () => [], // 초기 데이터
+        // retry: 0 // 조회 실패 시 몇번 진행 할지 카운트
     })
+
+    if ( isLoading ) {
+        return <BaseLoading />
+    }
 
 
     /*
